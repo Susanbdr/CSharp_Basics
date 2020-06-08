e0993a7bce419e9137802e686b28585a4bb4a362
Susan Joshi <susan.joshi@aggrowth.com>
2020-06-07 23:02:10 -0500

    Count of numbers between 1 and 100 that are divisible by 3 with no remainder.

---
+++ CSharp_Basics/LoopsSolutions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Basics
+{
+    public class LoopsSolutions
+    {
+        public static int NumbersDivisibleBy3()
+        {
+            var count = 0;
+
+            for (int i = 1; i <= 100; i++)
+                if (i % 3 == 0)
+                    count++;
+            
+            return count;
+        }
+    }
+}
--- CSharp_Basics/Program.cs
+++ CSharp_Basics/Program.cs
@@ -6,15 +6,7 @@ namespace CSharp_Basics
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter Speed Limit: ");
-            
-            var speedLimit = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter speed of your car: ");
-
-            var speedOfCar = Convert.ToInt32(Console.ReadLine());
-
-            ConditionalsSolution.SpeedLimit(speedLimit, speedOfCar);
+            Console.WriteLine(LoopsSolutions.NumbersDivisibleBy3());
         }
     }
 }
